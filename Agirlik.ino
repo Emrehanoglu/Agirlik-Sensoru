5e662dc51193140ed3830e9391fb20555002cbe8
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2021-04-25 16:24:37 +0300

    Add files via upload

---
+++ Agirlik.ino
@@ -0,0 +1,47 @@
+#include "HX711.h"
+
+HX711 scale(6, 5);
+
+float calibration_factor = 807;
+float units;
+float ounces;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
+
+  scale.set_scale();
+  scale.tare();
+}
+
+void loop() {
+
+  scale.set_scale(calibration_factor); 
+
+  Serial.print("Reading: ");
+  units = scale.get_units(), 10;
+  if (units < 0)
+  {
+    units = 0.00;
+  }
+  ounces = units * 0.035274;
+  Serial.print(units);
+  Serial.print(" grams"); 
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+  delay(100);
+
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+      calibration_factor += 1;
+    else if(temp == '-' || temp == 'z')
+      calibration_factor -= 1;
+  }
+}
